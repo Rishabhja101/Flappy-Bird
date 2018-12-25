31f4403290e6fd9b70d55805d14926a58d95e2f2
Rishabh Jain <rishabh101.99@gmail.com>
2018-12-24 20:53:02 -0800

    setup prefabs and began working on pipe placement

---
+++ Flappy Bird/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Pipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PlacePipe", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // places the pipes
+    private void PlacePipe()
+    {
+        GameObject newPipe = Instantiate(Pipe, transform.position, transform.rotation);
+
+        Invoke("PlacePipe", 5);
+    }
+}
